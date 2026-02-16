6bd01255e415b3f5df34807369e17a749505e66e
eagle <lefttolive@proton.me>
2026-02-16 16:34:02 -0300

    rumqttc: graceful shutdown without EventLoop API break

--- rumqttc/tests/reliability.rs
+++ rumqttc/tests/reliability.rs
@@ -95,7 +95,7 @@ async fn connection_should_timeout_on_time() {
 
     time::sleep(Duration::from_secs(1)).await;
     let options = MqttOptions::new("dummy", "127.0.0.1", 1880);
-    let (mut eventloop, _sender) = EventLoop::new(options, 5);
+    let mut eventloop = EventLoop::new(options, 5);
 
     let start = Instant::now();
     let o = eventloop.poll().await;
@@ -131,7 +131,7 @@ async fn idle_connection_triggers_pings_on_time() {
 
     // Create client eventloop and poll
     task::spawn(async move {
-        let (mut eventloop, _sender) = EventLoop::new(options, 5);
+        let mut eventloop = EventLoop::new(options, 5);
         run(&mut eventloop, false).await.unwrap();
     });
 
@@ -168,10 +168,11 @@ async fn some_outgoing_and_no_incoming_should_trigger_pings_on_time() {
     // start sending qos0 publishes. this makes sure that there is
     // outgoing activity but no incoming activity
     let (client, mut eventloop) = AsyncClient::new(options, 5);
+    let publisher = client.clone();
 
     // Start sending publishes
     task::spawn(async move {
-        start_requests(10, QoS::AtMostOnce, 1, client).await;
+        start_requests(10, QoS::AtMostOnce, 1, publisher).await;
     });
 
     // start the eventloop
@@ -210,7 +211,7 @@ async fn some_incoming_and_no_outgoing_should_trigger_pings_on_time() {
     options.set_keep_alive(keep_alive);
 
     task::spawn(async move {
-        let (mut eventloop, _sender) = EventLoop::new(options, 5);
+        let mut eventloop = EventLoop::new(options, 5);
         run(&mut eventloop, false).await.unwrap();
     });
 
@@ -254,7 +255,7 @@ async fn detects_halfopen_connections_in_the_second_ping_request() {
 
     time::sleep(Duration::from_secs(1)).await;
     let start = Instant::now();
-    let (mut eventloop, _sender) = EventLoop::new(options, 5);
+    let mut eventloop = EventLoop::new(options, 5);
     loop {
         if let Err(e) = eventloop.poll().await {
             match e {
@@ -512,7 +513,7 @@ async fn next_poll_after_connect_failure_reconnects() {
     });
 
     time::sleep(Duration::from_secs(1)).await;
-    let (mut eventloop, _sender) = EventLoop::new(options, 5);
+    let mut eventloop = EventLoop::new(options, 5);
 
     match eventloop.poll().await {
         Err(ConnectionError::ConnectionRefused(ConnectReturnCode::BadUserNamePassword)) => (),
