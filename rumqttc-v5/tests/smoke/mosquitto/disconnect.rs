803fc5c3092ae184e11e9aa60171c4b648d7efc5
eagle <lefttolive@proton.me>
2026-06-01 23:20:47 -0300

    lint

--- rumqttc-v5/tests/smoke/mosquitto/disconnect.rs
+++ rumqttc-v5/tests/smoke/mosquitto/disconnect.rs
@@ -267,7 +267,7 @@ async fn start_will_observer(
             match eventloop.poll().await {
                 Ok(Event::Incoming(Packet::SubAck(_))) => {
                     if let Some(tx) = suback_tx.take() {
-                    let _result = tx.send(());
+                        let _result = tx.send(());
                     }
                 }
                 Ok(Event::Incoming(Packet::Publish(publish))) => {
